126c89c7c749a74cb648f656a138e067497b318c
balavinay143 <61061724+balavinay143@users.noreply.github.com>
2022-11-21 16:59:27 +0530

    Update 3.py

--- typeface/3.py
+++ typeface/3.py
@@ -1,8 +1,8 @@
-n=int(input())
+a=int(input())
 l=['0','1','2','5','6','8','9']
 x=len(l)
-s=""
-while n>0:
-    s=s+l[n%x]
-    n//=x
-print(int(s[::-1]))
+q=""
+while a>0:
+    q=q+l[a%x]
+    a//=x
+print(int(q[::-1]))
